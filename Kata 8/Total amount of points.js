52f3f627779fe339fc1996d40dad2d19861635e1
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-07-03 15:55:47 +0300

    Total amount of points

---
+++ Kata 8/Total amount of points.js
@@ -0,0 +1,63 @@
+// Instructions
+
+// Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+// if x>y - 3 points
+// if x<y - 0 point
+// if x=y - 1 point
+// Notes:
+
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+
+function points(games) {
+	let count = 0;
+	games.forEach(item => {
+		if (item[0] > item[2]) count += 3;
+		if (item[0] === item[2]) count += 1;
+	})
+	return count;
+}
+
+
+// Sample Tests:
+// 1
+// describe("Basic tests", function(){
+// 2
+//   it("Testing for points(['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3'])", function(){
+// 3
+//     Test.assertEquals(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]) , 30);
+// 4
+//   });  
+// 5
+//   it("Testing for points(['1:1','2:2','3:3','4:4','2:2','3:3','4:4','3:3','4:4','4:4'])", function(){
+// 6
+//     Test.assertEquals(points(["1:1","2:2","3:3","4:4","2:2","3:3","4:4","3:3","4:4","4:4"]) , 10);
+// 7
+//   });
+// 8
+//   it("Testing for points(['0:1','0:2','0:3','0:4','1:2','1:3','1:4','2:3','2:4','3:4'])", function(){
+// 9
+//     Test.assertEquals(points(["0:1","0:2","0:3","0:4","1:2","1:3","1:4","2:3","2:4","3:4"]) , 0);
+// 10
+//   });
+// 11
+//   it("Testing for points(['1:0','2:0','3:0','4:0','2:1','1:3','1:4','2:3','2:4','3:4'])", function(){
+// 12
+//     Test.assertEquals(points(["1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"]) , 15);
+// 13
+//   });
+// 14
+//   it("Testing for points(['1:0','2:0','3:0','4:4','2:2','3:3','1:4','2:3','2:4','3:4'])", function(){
+// 15
+//     Test.assertEquals(points(["1:0","2:0","3:0","4:4","2:2","3:3","1:4","2:3","2:4","3:4"]) , 12);
+// 16
+//   });
+// 17
+// });
