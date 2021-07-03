b9c8fb0119ea6aa1d35f6dd4ea198755101e139c
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-07-03 18:38:45 +0300

    isReallyNaN

---
+++ Kata 8/isReallyNaN.js
@@ -0,0 +1,22 @@
+// Instructions
+
+// I've hit a few bugs in my Java/Type/Coffee-script code recently, and I've traced the problem back to the global isNaN function I was using. I had expected it to be more discerning, but it's returning true for undefined right now.
+
+// Write a function isReallyNaN that returns true only if passed in an argument that evalutes to NaN, and returns false otherwise.
+
+// Any solution is acceptable!
+
+
+const isReallyNaN = (val) => Number.isNaN(val);
+
+
+// Sample Tests:
+
+// describe("Tests", () => {
+// 	it("test", () => {
+//  Test.expect(isReallyNaN(37) === false)
+//  Test.expect(isReallyNaN('37') === false)
+//  Test.expect(isReallyNaN(NaN) === true)
+//  Test.expect(isReallyNaN(undefined) === false)
+// 	});
+//  });
