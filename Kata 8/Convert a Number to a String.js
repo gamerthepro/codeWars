4da0383413b2726816d8c82204d529ee914915db
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-07-06 13:45:01 +0300

    Convert a Number to a String

---
+++ Kata 8/Convert a Number to a String.js
@@ -0,0 +1,21 @@
+// Instructions
+
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+
+const numberToString = num => String(num);
+
+
+// Sample Tests:
+
+// describe("Tests", () => {
+// 	it("test", () => {
+//  Test.assertEquals(numberToString(67), '67');
+// 	});
+//  });
