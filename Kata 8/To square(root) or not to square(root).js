c5b7791970fe8fe703b0648722bb308ee245a640
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-07-06 16:15:58 +0300

    To square(root) or not to square(root)

--- Kata 8/To square(root) or not to square(root).js
+++ Kata 8/To square(root) or not to square(root).js
@@ -34,11 +34,9 @@ function squareOrSquareRoot(array) {
 // 	  var input = [ 4, 3, 9, 7, 2, 1 ];
 // 	  var expected = [ 2, 9, 3, 49, 4, 1 ];
 // 	  Test.assertSimilar(squareOrSquareRoot(input), expected);
-	  
 // 	  input = [ 100, 101, 5, 5, 1, 1 ];
 // 	  expected = [ 10, 10201, 25, 25, 1, 1 ];
 // 	  Test.assertSimilar(squareOrSquareRoot(input), expected);
-	  
 // 	  input = [ 1, 2, 3, 4, 5, 6 ];
 // 	  expected = [ 1, 4, 9, 2, 25, 36 ];
 // 	  Test.assertSimilar(squareOrSquareRoot(input), expected);
