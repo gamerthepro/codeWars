c85238a29d039ce9e939bde20e3a63b85da4921e
Artem Polonnikov <gamer.thepro@yandex.ru>
2021-08-09 16:38:11 +0300

    Who likes it

---
+++ Kata 6/Who likes it.js
@@ -0,0 +1,33 @@
+// Instructions
+
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function likes which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// likes [] -- must be "no one likes this"
+// likes ["Peter"] -- must be "Peter likes this"
+// likes ["Jacob", "Alex"] -- must be "Jacob and Alex like this"
+// likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
+// likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
+
+
+function likes(n) {
+	if (!n.length) return 'no one likes this';
+	if (n.length === 1) return `${n[0]} likes this`;
+	if (n.length === 2) return `${n[0]} and ${n[1]} like this`;
+	if (n.length === 3) return `${n[0]}, ${n[1]} and ${n[2]} like this`;
+	if (n.length > 3) return `${n[0]}, ${n[1]} and ${n.length - 2} others like this`;
+}
+
+
+// Sample Tests:
+
+// describe('example tests', function() {
+// 	it('should return correct text', function() {
+// 	  Test.assertEquals(likes([]), 'no one likes this');
+// 	  Test.assertEquals(likes(['Peter']), 'Peter likes this');
+// 	  Test.assertEquals(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+// 	  Test.assertEquals(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+// 	  Test.assertEquals(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
+// 	});
+//  });
