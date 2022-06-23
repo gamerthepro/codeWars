cfe9f72de3789383b100c2ffc160fca525bb5472
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-06-23 16:26:41 +0300

    Grasshopper - Debug sayHello

---
+++ Kata 8/Double Char.js
@@ -0,0 +1,27 @@
+// Double Char
+
+// Instructions
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+
+const doubleChar = str => str.split('').map(x => x + x).join('');
+
+
+// Sample Tests
+
+describe("doubleChar", function() {
+	it("works for some examples", function() {
+	  Test.assertEquals(doubleChar("abcd"), "aabbccdd");
+	  Test.assertEquals(doubleChar("Adidas"), "AAddiiddaass");
+	  Test.assertEquals(doubleChar("1337"), "11333377");
+	  Test.assertEquals(doubleChar("illuminati"), "iilllluummiinnaattii");
+	  Test.assertEquals(doubleChar("123456"), "112233445566");
+	  Test.assertEquals(doubleChar("%^&*("), "%%^^&&**((");
+	});
+ });
