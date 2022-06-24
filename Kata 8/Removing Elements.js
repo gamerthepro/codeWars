b7bb2bfff7258f7bb5c7770a8d30e6e6c0272ad0
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-06-24 20:40:38 +0300

    add Removing Elements

---
+++ Kata 8/Removing Elements.js
@@ -0,0 +1,32 @@
+// Removing Elements
+
+// Instructions
+
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+
+
+
+const removeEveryOther = arr => arr.filter((_, index) => index % 2 == 0)
+
+
+
+//  Sample Tests
+
+ const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.deepEqual(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']),['Hello', 'Hello Again']);
+    assert.deepEqual(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),[1, 3, 5, 7, 9]);
+    assert.deepEqual(removeEveryOther([[1, 2]]), [[1, 2]]);
+    assert.deepEqual(removeEveryOther([['Goodbye'], {'Great': 'Job'}]),[['Goodbye']]);
+  })
+});
