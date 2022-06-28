02dfcd99f082c58a986271311059b283f34c6511
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-06-28 08:34:21 +0300

    Function 3 - multiplying two numbers

---
+++ Kata 8/Function 3 - multiplying two numbers.js
@@ -0,0 +1,20 @@
+Function 3 - multiplying two numbers
+
+// Instructions
+
+// Implement a function which multiplies two numbers.
+
+
+const multiply = (x, y) => x * y;
+
+
+// Sample Tests
+
+const chai = require('chai');
+const assert = chai.assert;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(multiply(2, 3), 6);
+  });
+});
