804463d05e4d5631a4aabde22e81f85d0253f7a5
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-04 22:07:58 +0300

    Opposite number

---
+++ Kata 8/Opposite number.js
@@ -0,0 +1,22 @@
+// Opposite number
+
+// Instructions
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+const opposite = number => -number
+
+// Sample Tests
+
+describe("Tests", () => {
+	it("test", () => {
+ Test.assertEquals(opposite(1), -1,)
+	});
+ });
+ 
