30436d2561fe240b8df7be0f206afab12a30f64a
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-04 21:29:57 +0300

    L1: Set Alarm

---
+++ Kata 8/L1 Set Alarm.js
@@ -0,0 +1,31 @@
+// L1: Set Alarm
+
+// Instructions
+
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+
+const setAlarm = (employed, vacation) => employed && !vacation;
+
+const setAlarm = (employed, vacation) => employed == true && vacation == false ? true : false;
+
+
+// Sample Tests
+
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Test Suite",()=>{
+  it("Fixed tests",()=>{
+    assert.strictEqual(setAlarm(true, true),  false,"Should be false.");
+    assert.strictEqual(setAlarm(false,true),  false, "Should be false.");
+    assert.strictEqual(setAlarm(true, false), true,"Should be true.");
+  });
+});
