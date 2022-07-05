f9480ece6955d0934ef7234b44ab597c43290814
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-05 10:11:52 +0300

    Returning Strings

---
+++ Kata 8/Returning Strings.js
@@ -0,0 +1,25 @@
+// Returning Strings
+
+// Instructions
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+const greet = name => `Hello, ${name} how are you doing today?`
+
+
+
+// Sample Tests
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests",() =>{
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(greet("Ryan"), "Hello, Ryan how are you doing today?");
+    assert.strictEqual(greet("Shingles"), "Hello, Shingles how are you doing today?");
+  })
+})
