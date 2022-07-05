99928bbb9dccd24d6ab9b0220bad408804534296
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-05 11:49:14 +0300

    You Can't Code Under Pressure #1

---
+++ Kata 8/You Can't Code Under Pressure #1.js
@@ -0,0 +1,18 @@
+// You Can't Code Under Pressure #1
+
+// Instructions
+
+// Code as fast as you can! You need to double the integer and return it.
+
+
+const doubleInteger = i => 2 * i;
+
+
+
+// Sample Tests
+
+describe("Tests", () => {
+	it("test", () => {
+ Test.assertEquals(doubleInteger(2), 4);
+	});
+ });
