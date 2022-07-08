a84acf2cbfd9d3e5b741d9d78f8516ff09c1ff0d
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-08 11:33:16 +0300

    Beginner - Lost Without a Map

---
+++ Kata 8/Beginner - Lost Without a Map.js
@@ -0,0 +1,26 @@
+// Beginner - Lost Without a Map
+
+// Instructions
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+const maps = x => x.map(x => x * 2);
+
+
+// Sample Tests
+
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", ()=>{
+  it("Fixed tests", () => {
+    assert.deepEqual(maps([1, 2, 3]), [2, 4, 6]);
+    assert.deepEqual(maps([4, 1, 1, 1, 4]), [8, 2, 2, 2, 8]); 
+    assert.deepEqual(maps([2, 2, 2, 2, 2, 2]), [4, 4, 4, 4, 4, 4]); 
+  });
+});
