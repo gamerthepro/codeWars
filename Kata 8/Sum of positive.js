fc621806595ba0db4be8c085c1b13f99debd70a8
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-09 17:39:06 +0300

    Sum of positive

---
+++ Kata 8/Sum of positive.js
@@ -0,0 +1,29 @@
+// Sum of positive
+
+// Instructions
+
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+
+
+const positiveSum = arr => arr.reduce((x, y) => x + (y > 0 ? y : 0), 0)
+
+
+
+// Sample Tests
+
+const { assert } = require("chai")
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(positiveSum([1,2,3,4,5]),15);
+    assert.strictEqual(positiveSum([1,-2,3,4,5]),13);
+    assert.strictEqual(positiveSum([]),0);
+    assert.strictEqual(positiveSum([-1,-2,-3,-4,-5]),0);
+    assert.strictEqual(positiveSum([-1,2,3,4,-5]),9);
+    });
+  });
