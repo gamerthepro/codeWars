787b229a3b0e502b67d107eb2381674410acbddb
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-24 21:46:47 +0300

    Robinson Crusoe

---
+++ Kata 7/Robinson Crusoe.js
@@ -0,0 +1,83 @@
+// Robinson Crusoe
+
+// Instructions
+
+// Robinson Crusoe decides to explore his isle. On a sheet of paper he plans the following process.
+
+// His hut has coordinates origin = [0, 0]. From that origin he walks a given distance d on a line that has a given angle ang with the x-axis. He gets to a point A. (Angles are measured with respect to the x-axis)
+
+// From that point A he walks the distance d multiplied by a constant distmult on a line that has the angle ang multiplied by a constant angmult and so on and on.
+
+// We have d0 = d, ang0 = ang; then d1 = d * distmult, ang1 = ang * angmult etc ...
+
+// Let us suppose he follows this process n times. What are the coordinates lastx, lasty of the last point?
+
+// The function crusoe has parameters;
+
+// n : numbers of steps in the process
+// d : initial chosen distance
+// ang : initial chosen angle in degrees
+// distmult : constant multiplier of the previous distance
+// angmult : constant multiplier of the previous angle
+// crusoe(n, d, ang, distmult, angmult) should return lastx, lasty as an array or a tuple depending on the language.
+
+// Example:
+// crusoe(5, 0.2, 30, 1.02, 1.1) ->
+
+// The successive x are : 0.0, 0.173205, 0.344294, 0.511991, 0.674744, 0.830674 (approximately)
+
+// The successive y are : 0.0, 0.1, 0.211106, 0.334292, 0.47052, 0.620695 (approximately)
+
+// and
+
+// lastx: 0.8306737544381833
+// lasty: 0.620694691344071
+// A drawing:
+// alternative text
+
+// Successive points:
+
+// x: 0.0, 0.9659..., 1.8319..., 2.3319..., 1.8319...
+// y: 0.0, 0.2588..., 0.7588..., 1.6248..., 2.4908...
+// Note
+// Please could you ask before translating?
+
+
+
+function crusoe(n, d, ang, distmult, angmult) {
+	let x = 0,
+		 y = 0
+	ang *= Math.PI / 180
+ while (n > 0) {
+	x += d * Math.cos(ang)
+	y += d * Math.sin(ang)
+	ang *= angmult
+	d *= distmult
+	n--
+ }
+ return [x,y]
+}
+
+
+
+// Sample Tests
+
+const chai = require("chai");
+const assert = chai.assert;
+
+describe("crusoe",function() {
+
+function assert_fuzzy(n, d, ang, distmult, angmult, exp) {
+    let actual = crusoe(n, d, ang, distmult, angmult);
+    assert.approximately(actual[0], exp[0], 1e-9);
+    assert.approximately(actual[1], exp[1], 1e-9);
+}
+
+it("Basic tests code",function() {
+    assert_fuzzy(8, 0.22, 3, 1.01, 1.15, [1.814652098870, 0.164646220964]);
+    assert_fuzzy(29, 0.13, 21, 1.01, 1.09, [0.318341393410, 2.292862212314]);
+    assert_fuzzy(45, 0.10, 3, 1.01, 1.10, [2.689897523779, 2.477953232467]);
+    assert_fuzzy(14, 0.22, 20, 1.02, 1.14, [1.774076472485, 2.557008479031]);
+    assert_fuzzy(42, 0.11, 17, 1.02, 1.09, [0.528555980656, 2.196434600133]);
+    
+})})
